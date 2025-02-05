24f0134153abd90aebd98e11062b2d07153dc89c
generyand <generyan.dep@gmail.com>
2025-02-05 14:28:19 +0800

    changed route for search query

--- src/main/server/routes/user.route.ts
+++ src/main/server/routes/user.route.ts
@@ -6,7 +6,6 @@ router.get('/', getUsers)
 router.post('/', createUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
-router.get('/search', searchUser)
+router.get('/', searchUser)
 
 export default router
-
