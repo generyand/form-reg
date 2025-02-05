c41255e7ecbe554a3870fcade4f9602b66752309
generyand <generyan.dep@gmail.com>
2025-02-05 09:49:05 +0800

    changed User table name to users

---
+++ prisma/migrations/20250205014754_create_users_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
